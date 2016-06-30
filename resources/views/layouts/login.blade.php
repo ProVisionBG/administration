34f4ebd52da1b66ecfab184b217ffd69c1667399
Venelin Iliev <venelin@provision.bg>
2016-06-30 14:35:12 +0300

    login layout with new assets

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -7,18 +7,8 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-    <link rel="stylesheet" href="{{asset("/vendor/provision/administration/bower_components/bootstrap/dist/css/bootstrap.min.css")}}">
-    <link rel="stylesheet" href="{{asset("/vendor/provision/administration/bower_components/font-awesome/css/font-awesome.min.css")}}">
-    <link rel="stylesheet" href="{{asset("/vendor/provision/administration/bower_components/ionicons/css/ionicons.min.css")}}">
-    <link rel="stylesheet" href="{{asset("/vendor/provision/administration/bower_components/AdminLTE/dist/css/AdminLTE.min.css")}}">
-    <link rel="stylesheet" href="{{asset("/vendor/provision/administration/bower_components/iCheck/skins/square/blue.css")}}">
+    <link rel="stylesheet" href='{{asset("/vendor/provision/administration/css/all.css")}}'>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="{{asset("/vendor/provision/administration/bower_components/html5shiv/dist/html5shiv.min.js")}}"></script>
-    <script src="{{asset("/vendor/provision/administration/bower_components/respond/dest/respond.min.js")}}"></script>
-    <![endif]-->
 </head>
 <body class="hold-transition login-page">
 
@@ -34,9 +24,7 @@
 
 @yield('content')
 
-<script src="{{asset("/vendor/provision/administration/bower_components/jquery/dist/jquery.min.js")}}"></script>
-<script src="{{asset("/vendor/provision/administration/bower_components/bootstrap/dist/js/bootstrap.min.js")}}"></script>
-<script src="{{asset("/vendor/provision/administration/bower_components/iCheck/icheck.js")}}"></script>
+<script src="{{asset("/vendor/provision/administration/js/all.js")}}"></script>
 
 @yield('bottom_js')
 
