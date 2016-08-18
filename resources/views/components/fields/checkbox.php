d3d70f2a4bc03d6789ece072048b3852a0d4d3b1
Venelin Iliev <venelin@provision.bg>
2016-08-18 09:59:32 +0300

    remove default class from checkbox

--- resources/views/components/fields/checkbox.php
+++ resources/views/components/fields/checkbox.php
@@ -1,27 +1,33 @@
 <?php if ($showLabel && $showField): ?>
+<?php if ($options['wrapper'] !== false): ?>
+<div <?= $options['wrapperAttrs'] ?> >
+    <?php endif; ?>
+    <?php endif; ?>
+
+    <?php if ($showField): ?>
+
+        <?php
+        if (!empty($options['attr']['class']) && $options['attr']['class'] == 'form-control') {
+            $options['attr']['class'] = '';
+        }
+        echo Form::checkbox($name, $options['value'], $options['checked'], $options['attr']);
+        ?>
+
+        <?php include 'help_block.php' ?>
+    <?php endif; ?>
+
+    <?php if ($showLabel && $options['label'] !== false && $options['label_show']): ?>
+        <?php if ($options['is_child']): ?>
+            <label <?= $options['labelAttrs'] ?>><?= $options['label'] ?></label>
+        <?php else: ?>
+            <?= Form::label($name, $options['label'], $options['label_attr']) ?>
+        <?php endif; ?>
+    <?php endif; ?>
+
+    <?php include 'errors.php' ?>
+
+    <?php if ($showLabel && $showField): ?>
     <?php if ($options['wrapper'] !== false): ?>
-    <div <?= $options['wrapperAttrs'] ?> >
-    <?php endif; ?>
+</div>
 <?php endif; ?>
-
-<?php if ($showField): ?>
-    <?= Form::checkbox($name, $options['value'], $options['checked'], $options['attr']) ?>
-
-    <?php include 'help_block.php' ?>
-<?php endif; ?>
-
-<?php if ($showLabel && $options['label'] !== false && $options['label_show']): ?>
-    <?php if ($options['is_child']): ?>
-        <label <?= $options['labelAttrs'] ?>><?= $options['label'] ?></label>
-    <?php else: ?>
-        <?= Form::label($name, $options['label'], $options['label_attr']) ?>
-    <?php endif; ?>
-<?php endif; ?>
-
-<?php include 'errors.php' ?>
-
-<?php if ($showLabel && $showField): ?>
-    <?php if ($options['wrapper'] !== false): ?>
-    </div>
-    <?php endif; ?>
 <?php endif; ?>
