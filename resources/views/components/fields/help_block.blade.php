7c92c757c78aec7c64c6707d2c65190c58b3b4ae
Venelin Iliev <venelin@provision.bg>
2016-12-12 16:58:43 +0200

    help_block with attr array

--- resources/views/components/fields/help_block.blade.php
+++ resources/views/components/fields/help_block.blade.php
@@ -1,5 +1,9 @@
 <?php if ($options['help_block']['text'] && !$options['is_child']): ?>
-    <<?= $options['help_block']['tag'] ?> <?= $options['help_block']['helpBlockAttrs'] ?>>
-        <?= $options['help_block']['text'] ?>
-    </<?= $options['help_block']['tag'] ?>>
+<<?= $options['help_block']['tag'] ?> <? if (!empty($options['help_block']['attr'])) {
+    foreach ($options['help_block']['attr'] as $atr => $val) {
+        echo ' ' . $atr . '=' . $val;
+    }
+}?>>
+<?= $options['help_block']['text'] ?>
+</<?= $options['help_block']['tag'] ?>>
 <?php endif; ?>
