04c379fff2330e6ab04c116c4b6f09308eabbf61
Venelin Iliev <venelin@provision.bg>
2016-12-20 14:06:06 +0200

    adminLinkButton class fix

--- resources/views/components/form/admin_link_button.blade.php
+++ resources/views/components/form/admin_link_button.blade.php
@@ -1,4 +1,4 @@
-<a title="{{$name}}" href="{{@$attributes['href']}}" class="{{@$attributes['class']}}" target="{{@$attributes['target']}}">
+<a title="{{$name}}" href="{{@$attributes['href']}}" class="btn btn-sm {{@$attributes['class']}}" target="{{@$attributes['target']}}">
     @if(!empty($icon))
         <i class="fa fa-{{$icon}}" aria-hidden="true"></i>
     @else
