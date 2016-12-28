8b6cbadfd9cbd5229eb6d963b053749848bf7c81
Venelin Iliev <venelin@provision.bg>
2016-12-28 11:57:15 +0200

    rewrite help block with blade template

--- resources/views/components/fields/help_block.blade.php
+++ resources/views/components/fields/help_block.blade.php
@@ -1,9 +1,11 @@
-<?php if ($options['help_block']['text'] && !$options['is_child']): ?>
-<<?= $options['help_block']['tag'] ?> <? if (!empty($options['help_block']['attr'])) {
-    foreach ($options['help_block']['attr'] as $atr => $val) {
-        echo ' ' . $atr . '=' . $val;
-    }
-}?>>
-<?= $options['help_block']['text'] ?>
-</<?= $options['help_block']['tag'] ?>>
-<?php endif; ?>
+@if ($options['help_block']['text'] && !$options['is_child'])
+    <{{$options['help_block']['tag']}}
+    @if(!empty($options['help_block']['attr']))
+        @foreach ($options['help_block']['attr'] as $atr => $val)
+            {{$atr}}="{{$val}}"
+        @endforeach
+    @endif
+    >
+    {{$options['help_block']['text']}}
+    </{{$options['help_block']['tag']}}>
+@endif
