7fcdf633d2fba827f008a577e88f5304932c834f
Venelin Iliev <venelin@provision.bg>
2017-06-14 17:18:58 +0300

    tinymce blobCache fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -406,20 +406,24 @@
             // just in case, and visually hide it. And do not forget do remove it
             // once you do not need it anymore.
 
-            input.onchange = function () {
+            input.onchange = function() {
                 var file = this.files[0];
 
-                // Note: Now we need to register the blob in TinyMCEs image blob
-                // registry. In the next release this part hopefully won't be
-                // necessary, as we are looking to handle it internally.
-                var id = 'blobid' + (new Date()).getTime();
-                var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
-                var base64 = reader.result.split(',')[1];
-                var blobInfo = blobCache.create(id, file, base64);
-                blobCache.add(blobInfo);
+                var reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = function () {
+                    // Note: Now we need to register the blob in TinyMCEs image blob
+                    // registry. In the next release this part hopefully won't be
+                    // necessary, as we are looking to handle it internally.
+                    var id = 'blobid' + (new Date()).getTime();
+                    var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                    var base64 = reader.result.split(',')[1];
+                    var blobInfo = blobCache.create(id, file, base64);
+                    blobCache.add(blobInfo);
 
-                // call the callback and populate the Title field with the file name
-                cb(blobInfo.blobUri(), { title: file.name });
+                    // call the callback and populate the Title field with the file name
+                    cb(blobInfo.blobUri(), { title: file.name });
+                };
             };
 
             input.click();
