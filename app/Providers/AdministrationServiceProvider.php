2cdc709e4c7a0e71017e6ab26f76b61269514248
Venelin Iliev <venelin@provision.bg>
2018-05-11 11:32:48 +0300

    Add config for intervention/imagecache

--- app/Providers/AdministrationServiceProvider.php
+++ app/Providers/AdministrationServiceProvider.php
@@ -48,6 +48,7 @@ class AdministrationServiceProvider extends ServiceProvider {
             __DIR__ . '/../../config/provision_administration.php' => config_path('provision_administration.php'),
             __DIR__ . '/../../config/laravellocalization.php' => config_path('laravellocalization.php'),
             __DIR__ . '/../../config/entrust.php' => config_path('entrust.php'),
+            __DIR__ . '/../../config/imagecache.php' => config_path('imagecache.php'),
             __DIR__ . '/../../config/log-viewer.php' => config_path('log-viewer.php'),
             __DIR__ . '/../../config/laravel-form-builder.php' => config_path('laravel-form-builder.php'),
             __DIR__ . '/../../config/laravel-menu/settings.php' => config_path('laravel-menu/settings.php'),
